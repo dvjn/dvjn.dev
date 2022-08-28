dfaffc4cd98787000ef8ad3a329ebb47ada8d02f
Divya Jain <dvjn.dev+git@gmail.com>
2022-08-28 18:40:57 +0000

    :sparkles: new ui

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 120,
+    trailingComma: "all",
+    singleQuote: false,
+    bracketSpacing: true,
+    tabWidth: 4,
+    quoteProps: "consistent",
+    bracketSameLine: true,
+};

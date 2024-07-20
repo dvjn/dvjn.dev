250aeb930f91747ae462ef572803d04a5ee01ba2
Divya Jain <dvjn.dev+git@gmail.com>
2024-07-21 02:49:44 +0530

    :heavy_plus_sign: add rehype pretty code for better code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,27 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
+import { transformerCopyButton } from "@rehype-pretty/transformers";
 
 export default defineConfig({
+    markdown: {
+        syntaxHighlight: false,
+        rehypePlugins: [
+            [
+                rehypePrettyCode,
+                {
+                    theme: "github-dark-default",
+                    keepBackground: false,
+                    transformers: [
+                        transformerCopyButton({
+                            visibility: "hover",
+                            feedbackDuration: 1_000,
+                        }),
+                    ],
+                },
+            ],
+        ],
+    },
     integrations: [icon(), mdx()],
 });

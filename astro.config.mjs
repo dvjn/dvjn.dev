331125f4eadcd90405c40d617c6ee8e01e120aff
Divya Jain <dvjn.dev+git@gmail.com>
2024-07-21 20:34:17 +0530

    :sparkles: add reading time to posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,21 @@ import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import rehypePrettyCode from "rehype-pretty-code";
 import { transformerCopyButton } from "@rehype-pretty/transformers";
+import getReadingTime from "reading-time";
+import { toString } from "mdast-util-to-string";
+
+export function remarkReadingTime() {
+    return function (tree, { data }) {
+        const textOnPage = toString(tree);
+        const readingTime = getReadingTime(textOnPage);
+        data.astro.frontmatter.minutesRead = readingTime.text;
+    };
+}
 
 export default defineConfig({
     markdown: {
         syntaxHighlight: false,
+        remarkPlugins: [remarkReadingTime],
         rehypePlugins: [
             [
                 rehypePrettyCode,

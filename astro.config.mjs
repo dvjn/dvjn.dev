deff175a5e2d5ba396c024bede95bee45bedee9a
Divya Jain <git@dvjn.dev>
2026-03-03 16:25:46 +0530

    :mag: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,36 +7,39 @@ import getReadingTime from "reading-time";
 import { toString } from "mdast-util-to-string";
 import rehypeSlug from "rehype-slug";
 import rehypeAutoLinkHeadings from "rehype-autolink-headings";
+import sitemap from "@astrojs/sitemap";
+import vanityUrls from "./src/data/go_vanity_urls.json" assert { type: "json" };
+
+const vanityUrlPaths = new Set(vanityUrls.map((u) => `https://dvjn.dev/${u.name}/`));
 
 export function remarkReadingTime() {
-    return function (tree, { data }) {
-        const textOnPage = toString(tree);
-        const readingTime = getReadingTime(textOnPage, { wordsPerMinute: 100 });
-        data.astro.frontmatter.minutesRead = readingTime.text;
-    };
+  return function (tree, {
+    data
+  }) {
+    const textOnPage = toString(tree);
+    const readingTime = getReadingTime(textOnPage, {
+      wordsPerMinute: 100
+    });
+    data.astro.frontmatter.minutesRead = readingTime.text;
+  };
 }
 
+// https://astro.build/config
 export default defineConfig({
-    markdown: {
-        syntaxHighlight: false,
-        remarkPlugins: [remarkReadingTime],
-        rehypePlugins: [
-            [
-                rehypePrettyCode,
-                {
-                    theme: "aurora-x",
-                    keepBackground: false,
-                    transformers: [
-                        transformerCopyButton({
-                            visibility: "hover",
-                            feedbackDuration: 1_000,
-                        }),
-                    ],
-                },
-            ],
-            rehypeSlug,
-            [rehypeAutoLinkHeadings, { behavior: "wrap" }],
-        ],
-    },
-    integrations: [icon(), mdx()],
+  site: "https://dvjn.dev",
+  markdown: {
+    syntaxHighlight: false,
+    remarkPlugins: [remarkReadingTime],
+    rehypePlugins: [[rehypePrettyCode, {
+      theme: "aurora-x",
+      keepBackground: false,
+      transformers: [transformerCopyButton({
+        visibility: "hover",
+        feedbackDuration: 1_000
+      })]
+    }], rehypeSlug, [rehypeAutoLinkHeadings, {
+      behavior: "wrap"
+    }]]
+  },
+  integrations: [icon(), mdx(), sitemap({ filter: (page) => !vanityUrlPaths.has(page) })]
 });
